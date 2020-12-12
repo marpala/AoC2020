24ee617273f09c45da2aa8625fe4a53ba0e2d55e
Marc <marpala@users.noreply.github.com>
2020-12-12 22:34:55 +0100

    found missing day

--- AoC2020-Python/day08.py
+++ AoC2020-Python/day08.py
@@ -5,8 +5,47 @@ class AoCDay8(AoCSol):
     
     def parse_input(self):
         with open(self.path) as f:
-            return [(line[:3], line[4:].replace('\n', '')) for line in f]
+            return [(line[:3], int(line[4:].replace('\n', ''))) for line in f]
+    
+    def simulate(self):
+        accumulator = 0
+        history = set()
+        index = 0
+        while index < len(self.input):
+            if index in history:
+                return accumulator, True
 
+            history.add(index)
+            ins, val = self.input[index]
+            if ins == 'acc':
+                accumulator += val
+            elif ins == 'jmp':
+                index += val
+                continue
+            index += 1
+        return accumulator, False
 
-    def solve1:
-        ff
+    def stuck(self):
+        return self.simulate()[1]
+
+    def solve1(self):
+        return self.simulate()[0]
+    
+    def solve2(self):
+        inpt_og = self.input.copy()
+
+        for i in range(len(self.input)):
+            if self.input[i][0] == 'jmp':
+                self.input[i] = ('nop', self.input[i][1])
+            elif self.input[i][0] == 'nop':
+                self.input[i] = ('jmp', self.input[i][1])
+            else:
+                continue
+            
+            if not self.stuck():
+                return self.solve1()
+            else:
+                self.input = inpt_og.copy()
+
+inputpath = '/input/day8.txt'
+print(AoCDay8(inputpath))
